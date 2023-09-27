85a4d132251fe49928700736b986b1bd0e515bb1
ClaudioHurlingham <103459164+ClaudioHurlingham@users.noreply.github.com>
2023-09-27 19:45:45 -0300

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,22 @@
 //const pantalla = document.querySelector('pantalla');1
-const pantalla= document.querySelector("pantalla");
-const botones = document.querySelectorAll('input')
+const pantalla= document.querySelector(".pantalla");
+const botones = document.querySelectorAll('input');
 
 // v0
 
 botones.forEach(boton =>{
-    boton.onclick = ()=>{
-        if(boton.id=='eliminar'){
+    boton.addEventListener('click',()=>{
+        if(boton.value=='C'){
             pantalla.textContent = '';
-        }else if(boton.id == 'borrar'){
+        }else if(boton.value == 'CE'){
             pantalla.textContent = pantalla.textContent.slice(o,-1)
-        }else if(pantalla.textContent==''&& boton.id=='='){
-
+        }else if(pantalla.textContent != '' && boton.value == '='){
+            pantalla.textContent = eval(pantalla.textContent);
+        }else if(pantalla.textContent == '' && boton.value=='='){
             pantalla.textContent= 'Vacio!';
             setTimeout(()=>(pantalla.textContent=''),2000);
-        }else if(pantalla.textContent!=="cosa"){
-            Tucomando;
         }else{
-            pantalla.textContent += boton.id;
+            pantalla.textContent += boton.value;
         }
     }
-
-})
+)});
